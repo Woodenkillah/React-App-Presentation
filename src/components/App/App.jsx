0acdac4e12bf33201900a64c1e0ec6a605e6076f
Roman Boychuk <drsanta1@yandex.ru>
2021-05-06 19:39:15 +0300

    initial configuration

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+
+  return (
+    <div className={styles.App}>
+      <h1>Amazing Webpack!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+      favicon: ''
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './public'),
+    open: true,
+    hot: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
+};
